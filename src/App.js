26b9268342f0bf724b117bd8a68fa8398cf9d132
Raj Barnwal <raj.barnwal@tothenew.com>
2020-08-23 15:51:09 +0530

    FInal setup done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './assets/stylesheet/style.scss';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="header p30">
+                UI Global components
+            </div>
+        </div>
+    );
+}
+
+export default App;
